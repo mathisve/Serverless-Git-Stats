ef9f283239dfd25aa94ff30389c07d49ee439534
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2020-10-13 09:02:39 +0200

    Moved data granularity to backend

--- example/script.js
+++ example/script.js
@@ -1,63 +1,44 @@
-async function main() {
-  graph(0, [], [], 0, 0);
+async function main () {
+  graph(0, [], [], 0);
 
-  const url = "https://whqqnvb6ge.execute-api.eu-central-1.amazonaws.com/prod/git-stats";
-  resp = await fetch(url)
-      .then((res) => {
-        if(res.status == 200) {
-            return res.json();   //works just fine
-        }
-      })
-      .catch(error => console.log(error.message));
+  resp = await $.post("https://whqqnvb6ge.execute-api.eu-central-1.amazonaws.com/prod/git-stats", JSON.stringify({link: "https://github.com/users/mathisco-01/contributions",granularity: 7}));
+  graph(resp.total, resp.data.count, resp.data.date, 5);
 
-  var dataLength = resp.data.count.length
-  var newData = [];
-  var newLabels = [];
 
-  const dataGranularity = 5;
-  const labelGranularity = dataLength/dataGranularity/10;
 
-  for (i = 0; i < dataLength; i++) {
-    if (i % dataGranularity == 0) {
-      newData.push(resp.data.count[i]);
-      newLabels.push(resp.data.date[i]);
-    }
-  }
+};
 
-  graph(dataLength, newData, newLabels, dataGranularity, labelGranularity);
-
-function graph(dataLength, data, labels, dataGranularity, labelGranularity) {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var lineChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: [{
-          data: data,
-          backgroundColor: [
-               'rgba(0, 102, 255, 0.2)',
-           ],
-         borderColor: [
-           'rgba(0, 51, 204, 1)',
+function graph(total, data, labels, labelGranularity) {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var lineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [{
+        data: data,
+        backgroundColor: [
+             'rgba(0, 102, 255, 0.2)',
          ],
-        }],
+       borderColor: [
+         'rgba(0, 51, 204, 1)',
+       ],
+      }],
+    },
+    options: {
+      title: {
+        display: true,
+        text: `${total} Git contributions in the past year`
       },
-      options: {
-        title: {
-          display: true,
-          text: `${dataLength} Git contributions in the past year`
-        },
-        legend: {
-          display: false,
-        },
-        scales: {
-          xAxes: [{
-            ticks: {
-              maxTicksLimit: labelGranularity
-            }
-          }]
-        }
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            maxTicksLimit: data.length / labelGranularity
+          }
+        }]
       }
-    })
-  };
-}
+    }
+  })
+};
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from bs4 import BeautifulSoup
 import json
 import os
 
+import logging
+
 import requests
 
-def scrape(link):
+def scrape(link, granularity):
     resp = requests.get(link)
     days = BeautifulSoup(resp.text, "html.parser").find_all("rect")
 
@@ -16,10 +18,11 @@ def scrape(link):
                 }
 
     c = 0
-    for day in days:
-        c += int(day["data-count"])
-        jsonObj["data"]["date"].append(day["data-date"])
-        jsonObj["data"]["count"].append(c)
+    for i in range(len(days)):
+        c += int(days[i]["data-count"])
+        if i % granularity == 0:
+            jsonObj["data"]["date"].append(days[i]["data-date"])
+            jsonObj["data"]["count"].append(c)
 
     jsonObj["total"] = c
 
@@ -33,11 +36,31 @@ def scrape(link):
     }
 
 def handler(event, context):
-    # Hardcoded URL so users can't use your lambda function for their own profiles!
-    link = os.environ["LINK"]
-    return scrape(link)
+    jsobBody = json.loads(event["body"])
+
+    try:
+        if jsobBody["link"] in os.environ["LINK"].split(","):
+            link = jsobBody["link"]
+        else:
+            link = os.environ["LINK"]
+
+        granularity = int(jsobBody["granularity"])
+
+        return scrape(link, granularity)
+    except Exception as e:
+        logging.warning(e)
+        return e
+        return {
+            'statusCode': 400,
+            'headers': {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": True
+            },
+        }
 
 if __name__ == "__main__":
-    resp = scrape("https://github.com/users/Mathisco-01/contributions")
+    link = "https://github.com/users/Mathisco-01/contributions"
+    granularity = 10
+    resp = scrape(link, granularity)
 
     print(resp)
